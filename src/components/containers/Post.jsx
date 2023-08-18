5041370b0cc5c3a2d5c7df77122f98b802a114ae
Ignacio Bergantiños <bergantinos.ignacio@gmail.com>
2023-08-18 16:51:19 -0300

    update: change post div for an article

--- src/components/containers/Post.jsx
+++ src/components/containers/Post.jsx
@@ -1,11 +1,11 @@
 const Post = ({ date, title, description }) => {
 	return (
-		<div className="post">
+		<article className="post">
 			<p className="date">{date}</p>
 			<h2 className="title">{title}</h2>
 			<p className="description">{description}</p>
 			<a href="">Read article</a>
-		</div>
+		</article>
 	);
 };
 
